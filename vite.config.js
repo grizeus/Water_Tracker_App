8268d349ca1b77c0fcc8dcb8c722fed2a8fde031
LazyStudent23 <wizard.alex2016@gmail.com>
2025-02-02 22:07:46 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,17 @@
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react-swc';
+
+// export default defineConfig({
+//   plugins: [react()],
+//   resolve: {
+//     alias: {
+//       src: '/src',
+//       components: '/src/components',
+//       pages: '/src/pages',
+//     },
+//   },
+//   base: '/',
+// });
 
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
@@ -7,5 +21,13 @@ export default defineConfig({
   plugins: [react()],
   build: {
     sourcemap: true,
-  }
+  },
+  resolve: {
+    alias: {
+      src: '/src',
+      components: '/src/components',
+      pages: '/src/pages',
+    },
+  },
+  base: '/',
 });
