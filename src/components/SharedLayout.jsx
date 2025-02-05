877e813d041b8ef2e142a822ab967b2a48ca78d4
LazyStudent23 <wizard.alex2016@gmail.com>
2025-02-05 22:56:23 +0200

    add general layout for all routes

---
+++ src/components/SharedLayout.jsx
@@ -0,0 +1,27 @@
+import { Header, Loader } from 'components';
+import { Suspense } from 'react';
+import { Outlet } from 'react-router-dom';
+import { ToastContainer, Zoom } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { Wrapper } from './Footer/Footer.styled';
+
+const SharedLayout = () => {
+  return (
+    <Wrapper>
+      <Header />
+      <main>
+        <Suspense fallback={<Loader />}>
+          <Outlet />
+        </Suspense>
+      </main>
+      <ToastContainer
+        position="top-right"
+        autoClose={3000}
+        draggable={false}
+        transition={Zoom}
+      />
+    </Wrapper>
+  );
+};
+
+export default SharedLayout;
