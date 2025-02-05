64f82d9a6ba4521233f5e5b1b7a653f7465ff1d9
Supsayan9 <127434945+Supsayan9@users.noreply.github.com>
2025-02-05 07:31:00 +0100

    +

--- src/components/common/Section/Section.jsx
+++ src/components/common/Section/Section.jsx
@@ -1,1 +1,9 @@
-import { Container } from "components"
+import { Container } from 'components';
+
+export const Section = ({ className, children }) => {
+  return (
+    <section className={className}>
+      <Container>{children}</Container>
+    </section>
+  );
+};
