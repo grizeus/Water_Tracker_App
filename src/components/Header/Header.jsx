e1545b37166ba77e8abeb91508e4207fc6963089
Nataliya Yakovyna <yakovina.n13@gmail.com>
2025-02-06 14:38:10 +0200

    add header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,1 +1,20 @@
-export const Header = () => {};
+import { Container, Logo, UserLogo } from 'components';
+import { UserAuth } from './UserAuth/UserAuth.jsx';
+import { useSelector } from 'react-redux';
+import { selectIsLoggedIn } from '../../redux/auth/authSelectors';
+import { ContainerHeader, WrapHeader } from './Header.styled';
+
+export const Header = () => {
+  const isLoggedIn = useSelector(selectIsLoggedIn);
+
+  return (
+    <header>
+      <Container>
+        <ContainerHeader>
+          <Logo />
+          <WrapHeader>{isLoggedIn ? <UserLogo /> : <UserAuth />}</WrapHeader>
+        </ContainerHeader>
+      </Container>
+    </header>
+  );
+};
