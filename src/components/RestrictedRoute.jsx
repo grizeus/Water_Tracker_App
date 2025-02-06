2e563537b15fbb3d26ae88197ba4689e945d768e
LazyStudent23 <wizard.alex2016@gmail.com>
2025-02-06 22:04:54 +0200

    add private & restricted route logic

--- src/components/RestrictedRoute.jsx
+++ src/components/RestrictedRoute.jsx
@@ -1,3 +1,11 @@
-const RestrictedRoute = () => {};
+import { useSelector } from 'react-redux';
+import { Navigate } from 'react-router-dom';
+import { selectIsLoggedIn } from '../redux/auth/authSelectors';
+
+const RestrictedRoute = ({ component: Component, redirectTo }) => {
+  const isLoggedIn = useSelector(selectIsLoggedIn);
+
+  return isLoggedIn ? <Navigate to={redirectTo} /> : Component;
+};
 
 export default RestrictedRoute;
