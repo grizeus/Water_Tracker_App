9394603996d69804bdd7ba0a37c1351cc1162d22
Nataliya Yakovyna <yakovina.n13@gmail.com>
2025-02-07 23:34:21 +0200

    add setting form

--- src/components/Header/SettingModal/SettingModal.jsx
+++ src/components/Header/SettingModal/SettingModal.jsx
@@ -1,9 +1,35 @@
-// import { BaseModalWindow } from '../../common/BaseModalWindow/BaseModalWindow.jsx';
-import { Loader } from '../../common/Loader/Loader.jsx';
-import { Field, Form, Formik } from 'formik';
-import { ModalWrap, FormText } from './SettingModal.styled.jsx';
-import * as Yup from 'yup';
 import { BaseModalWindow } from '../../common/BaseModalWindow/BaseModalWindow.jsx';
+
+import { Form, Formik } from 'formik';
+import { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import sprite from 'src/assets/images/sprite/sprite.svg';
+import * as Yup from 'yup';
+import { editUserInfoThunk } from '../../../redux/auth/authOperations';
+import { selectIsLoading } from '../../../redux/root/rootSelectors';
+
+import {
+  DesktopFormWrap,
+  DesktopGenderWrap,
+  DesktopPasswordWrap,
+  FormField,
+  FormText,
+  IconBtn,
+  IconDownload,
+  Input,
+  LastPasswordFormField,
+  ModalWrap,
+  PasswordFormField,
+  PasswordIcon,
+  PasswordInputWrap,
+  PasswordLabel,
+  PasswordText,
+  SaveBtn,
+  SaveBtnWrap,
+  StyledErrorMessage,
+  StyledErrorText,
+  StyledLabel,
+} from './SettingModal.styled';
 
 const settingFormValidationSchema = Yup.object().shape({
   gender: Yup.string(),
@@ -33,10 +59,218 @@ const settingFormValidationSchema = Yup.object().shape({
   ),
 });
 
-export const SettingModal = () => {
+export const SettingModal = ({ onClose, onShow }) => {
+  const dispatch = useDispatch();
+  const { isLoading } = useSelector(selectIsLoading);
+  const [isPasswordShown, setIsPasswordShown] = useState(false);
+
+  const initialValues = {
+    outdatedPassword: '',
+    newPassword: '',
+    repeatedPassword: '',
+  };
+
+  const handleSubmit = (values, actions) => {
+    if (values.outdatedPassword && !values.newPassword) {
+      return;
+    }
+
+    const { gender, name, email, outdatedPassword, newPassword } = values;
+
+    const formData = {
+      gender,
+      name,
+      email,
+      outdatedPassword,
+      newPassword,
+    };
+
+    const dataSend = {};
+
+    Object.entries(formData).forEach(([key, value]) => {
+      if (value) {
+        dataSend[key] = value;
+      }
+    });
+
+    dispatch(editUserInfoThunk(dataSend)).then(data => {
+      if (!data.error) {
+        onClose();
+        actions.resetForm();
+      }
+    });
+  };
+
+  const handlePasswordVisibility = () => {
+    setIsPasswordShown(previsPasswordShown => !previsPasswordShown);
+  };
+
   return (
     <>
-      <BaseModalWindow></BaseModalWindow>
+      <BaseModalWindow onClose={onClose} onShow={onShow} title="Setting">
+        <ModalWrap>
+          {
+            <Formik
+              initialValues={initialValues}
+              validationSchema={settingFormValidationSchema}
+              onSubmit={handleSubmit}
+            >
+              {({ values, errors, touched }) => (
+                <Form>
+                  <DesktopFormWrap>
+                    <DesktopGenderWrap>
+                      <FormField>
+                        <StyledLabel htmlFor="username">Your name</StyledLabel>
+                        <Input
+                          type="text"
+                          id="username"
+                          name="name"
+                          className={
+                            errors.name && touched.name ? 'error-input' : null
+                          }
+                          placeholder={values.name}
+                        />
+                        <StyledErrorMessage component="p" name="name" />
+                      </FormField>
+                      <div>
+                        <StyledLabel htmlFor="email">E-mail</StyledLabel>
+                        <Input
+                          type="email"
+                          id="email"
+                          name="email"
+                          className={
+                            errors.email && touched.email ? 'error-input' : null
+                          }
+                          placeholder={values.email}
+                        />
+                        <StyledErrorMessage component="p" name="email" />
+                      </div>
+                    </DesktopGenderWrap>
+                    <DesktopPasswordWrap>
+                      <PasswordText>Password</PasswordText>
+                      <PasswordFormField>
+                        <PasswordLabel htmlFor="oldPassword">
+                          Outdated password:
+                        </PasswordLabel>
+                        <PasswordInputWrap>
+                          <Input
+                            type={isPasswordShown ? 'text' : 'password'}
+                            id="oldPassword"
+                            name="outdatedPassword"
+                            className={
+                              errors.outdatedPassword &&
+                              touched.outdatedPassword
+                                ? 'error-input'
+                                : null
+                            }
+                            placeholder="Password"
+                          />
+                          <IconBtn
+                            type="button"
+                            onClick={handlePasswordVisibility}
+                          >
+                            {isPasswordShown ? (
+                              <PasswordIcon>
+                                <use href={`${sprite}#icon-to-open`}></use>
+                              </PasswordIcon>
+                            ) : (
+                              <PasswordIcon>
+                                <use href={`${sprite}#icon-to-hide`}></use>
+                              </PasswordIcon>
+                            )}
+                          </IconBtn>
+                        </PasswordInputWrap>
+                        <StyledErrorMessage
+                          component="p"
+                          name="outdatedPassword"
+                        />
+                      </PasswordFormField>
+                      <PasswordFormField>
+                        <PasswordLabel htmlFor="password">
+                          New Password:
+                        </PasswordLabel>
+                        <PasswordInputWrap>
+                          <Input
+                            type={isPasswordShown ? 'text' : 'password'}
+                            id="password"
+                            name="newPassword"
+                            className={
+                              (errors.newPassword && touched.newPassword) ||
+                              (values.outdatedPassword && !values.newPassword)
+                                ? 'error-input'
+                                : null
+                            }
+                            placeholder="Password"
+                          />
+                          <IconBtn
+                            type="button"
+                            onClick={handlePasswordVisibility}
+                          >
+                            {isPasswordShown ? (
+                              <PasswordIcon>
+                                <use href={`${sprite}#icon-to-open`}></use>
+                              </PasswordIcon>
+                            ) : (
+                              <PasswordIcon>
+                                <use href={`${sprite}#icon-to-hide`}></use>
+                              </PasswordIcon>
+                            )}
+                          </IconBtn>
+                        </PasswordInputWrap>
+                        {values.outdatedPassword && !values.newPassword && (
+                          <StyledErrorText>
+                            Please, enter new password
+                          </StyledErrorText>
+                        )}
+                        <StyledErrorMessage component="p" name="newPassword" />
+                      </PasswordFormField>
+                      <LastPasswordFormField>
+                        <PasswordLabel htmlFor="repeatedPassword">
+                          Repeat new password:
+                        </PasswordLabel>
+                        <PasswordInputWrap>
+                          <Input
+                            type={isPasswordShown ? 'text' : 'password'}
+                            id="repeatedPassword"
+                            name="repeatedPassword"
+                            className={
+                              values.newPassword !== values.repeatedPassword
+                                ? 'error-input'
+                                : null
+                            }
+                            placeholder="Password"
+                          />
+                          <IconBtn
+                            type="button"
+                            onClick={handlePasswordVisibility}
+                          >
+                            {isPasswordShown ? (
+                              <PasswordIcon>
+                                <use href={`${sprite}#icon-to-open`}></use>
+                              </PasswordIcon>
+                            ) : (
+                              <PasswordIcon>
+                                <use href={`${sprite}#icon-to-hide`}></use>
+                              </PasswordIcon>
+                            )}
+                          </IconBtn>
+                        </PasswordInputWrap>
+                        <StyledErrorMessage
+                          component="p"
+                          name="repeatedPassword"
+                        />
+                      </LastPasswordFormField>
+                    </DesktopPasswordWrap>
+                  </DesktopFormWrap>
+                  <SaveBtnWrap>
+                    <SaveBtn type="submit">Save</SaveBtn>
+                  </SaveBtnWrap>
+                </Form>
+              )}
+            </Formik>
+          }
+        </ModalWrap>
+      </BaseModalWindow>
     </>
   );
 };
--- src/components/Header/SettingModal/SettingModal.styled.jsx
+++ src/components/Header/SettingModal/SettingModal.styled.jsx
@@ -1,4 +1,113 @@
 import styled from '@emotion/styled';
+import { Field, ErrorMessage } from 'formik';
+
+export const FormText = styled.p`
+  color: ${({ theme }) => theme.color.black};
+  font-size: 18px;
+  font-weight: 500;
+  line-height: 1.11; /* 111.111% */
+`;
+
+export const StyledLabel = styled.label`
+  display: inline-block;
+  margin-bottom: 8px;
+  color: ${({ theme }) => theme.color.black};
+  font-size: 18px;
+  font-weight: 500;
+  line-height: 1.11; /* 111.111% */
+`;
+
+export const IconDownload = styled.svg`
+  width: 16px;
+  height: 16px;
+  fill: none;
+  stroke: ${({ theme }) => theme.color.accent};
+  stroke-linecap: round;
+  stroke-linejoin: round;
+`;
+
+export const IconBtn = styled.button`
+  border: none;
+  background-color: transparent;
+  padding: 0;
+`;
+
+export const PasswordInputWrap = styled.div`
+  position: relative;
+  margin-top: 8px;
+`;
+
+export const PasswordIcon = styled(IconDownload)`
+  position: absolute;
+  right: 10px;
+  top: 50%;
+  transform: translateY(-50%);
+`;
+
+export const FormField = styled.div`
+  margin-bottom: 24px;
+
+  .error-input {
+    border: 1px solid ${({ theme }) => theme.color.secondaryRed};
+    color: ${({ theme }) => theme.color.secondaryRed};
+
+    &:focus {
+      color: ${({ theme }) => theme.color.secondaryRed};
+    }
+
+    &::placeholder {
+      color: ${({ theme }) => theme.color.secondaryRed};
+    }
+  }
+`;
+
+export const PasswordFormField = styled(FormField)`
+  height: 82px;
+  @media screen and (max-width: 767px) {
+    margin-bottom: 28px;
+  }
+  @media screen and (min-width: ${({ theme }) => theme.breakpoint.tablet}) {
+    margin-bottom: 12px;
+  }
+`;
+
+export const Input = styled(Field)`
+  width: 100%;
+  height: 44px;
+  padding-top: 12px;
+  padding-bottom: 12px;
+  padding-left: 10px;
+  padding-right: 10px;
+  border-radius: 6px;
+  border: 1px solid ${({ theme }) => theme.color.secondaryLightBlue};
+  color: ${({ theme }) => theme.color.secondaryLightBlue};
+  font-size: 16px;
+  line-height: 1.25;
+  outline: transparent; /* 125% */
+  background-color: ${({ theme }) => theme.color.white};
+
+  &:focus {
+    color: ${({ theme }) => theme.color.accent};
+  }
+
+  &::placeholder {
+    color: ${({ theme }) => theme.color.secondaryLightBlue};
+  }
+`;
+
+export const StyledErrorMessage = styled(ErrorMessage)`
+  margin-top: 4px;
+  color: ${({ theme }) => theme.color.secondaryRed};
+  font-size: 14px;
+  line-height: 18px; /* 128.571% */
+`;
+
+export const StyledErrorText = styled.p`
+  margin-top: 4px;
+  color: ${({ theme }) => theme.color.secondaryRed};
+  font-size: 14px;
+  line-height: 18px; /* 128.571% */
+`;
 
 export const ModalWrap = styled.div`
   padding-left: 12px;
@@ -17,9 +126,104 @@ export const ModalWrap = styled.div`
     min-width: 1008px;
   }
 `;
-export const FormText = styled.p`
+
+export const PasswordText = styled(FormText)`
+  margin-bottom: 12px;
+`;
+
+export const PasswordLabel = styled.label`
+  margin-bottom: 8px;
   color: ${({ theme }) => theme.color.black};
-  font-size: 18px;
+  font-size: 16px;
+  line-height: 1.25; /* 125% */
+`;
+export const SaveBtnWrap = styled.ul`
+  li {
+    @media screen and (max-width: 767px) {
+      &:not(:last-child) {
+        margin-bottom: 24px;
+      }
+    }
+  }
+
+  @media screen and (min-width: ${({ theme }) => theme.breakpoint.tablet}) {
+    display: flex;
+    align-items: center;
+    flex-direction: row-reverse;
+    justify-content: space-between;
+  }
+`;
+
+export const SaveBtn = styled.button`
+  min-width: 100%;
+  padding: 8px 30px;
+  border-radius: 10px;
+  background: ${({ theme }) => theme.color.accent};
+  box-shadow: 0px 4px 8px 0px rgba(64, 123, 255, 0.34);
+  color: #fff;
+  font-size: 16px;
   font-weight: 500;
-  line-height: 1.11; /* 111.111% */
+  line-height: 1.25; /* 125% */
+  transition: box-shadow ${({ theme }) => theme.transition.main};
+
+  &:is(:hover, :focus) {
+    box-shadow: 0px 4px 14px 0px rgba(64, 123, 255, 0.54);
+  }
+
+  @media (min-width: ${({ theme }) => theme.breakpoint.tablet}) {
+    min-width: 160px;
+    padding: 10px 30px;
+    font-size: 18px;
+    line-height: 24px; /* 133.333% */
+  }
+`;
+
+export const DesktopFormWrap = styled.div`
+  margin-bottom: 24px;
+
+  @media screen and (min-width: ${({ theme }) => theme.breakpoint.desktop}) {
+    display: flex;
+    gap: 24px;
+  }
+`;
+
+export const DesktopPasswordWrap = styled.div`
+  @media screen and (min-width: ${({ theme }) => theme.breakpoint.mobile}) {
+    min-height: 320px;
+  }
+  @media screen and (min-width: ${({ theme }) => theme.breakpoint.tablet}) {
+    width: 392px;
+  }
+`;
+
+export const DesktopGenderWrap = styled.div`
+  @media screen and (max-width: 1439px) {
+    margin-bottom: 24px;
+  }
+  @media screen and (min-width: ${({ theme }) => theme.breakpoint.tablet}) {
+    width: 392px;
+  }
+  @media screen and (min-width: ${({ theme }) => theme.breakpoint.desktop}) {
+    margin-bottom: 0px;
+  }
+`;
+
+export const LastPasswordFormField = styled.div`
+  @media screen and (max-width: 767px) {
+    height: 92px;
+  }
+
+  @media screen and (min-width: ${({ theme }) => theme.breakpoint.tablet}) {
+    height: 82px;
+  }
+`;
+
+export const GenderFormField = styled.div`
+  @media screen and (max-width: 1439px) {
+    margin-bottom: 24px;
+  }
+
+  @media screen and (min-width: ${({ theme }) => theme.breakpoint.desktop}) {
+    margin-bottom: 52px;
+  }
 `;
