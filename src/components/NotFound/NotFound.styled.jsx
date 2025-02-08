2b9e26b154585c71cdf9fe44af564b0c2ebe4cf2
Nataliia Hordiienko <Natalihordienko@gmail.com>
2025-02-09 00:33:06 +0100

    fixed errorImage and styles

--- src/components/NotFound/NotFound.styled.jsx
+++ src/components/NotFound/NotFound.styled.jsx
@@ -7,7 +7,8 @@ export const ErrorBtn = styled.button`
   border-radius: 10px;
   font-size: 18px;
   height: 44px;
-  margin-left: auto;
+  display: block;
+  margin: 0 auto;
   box-shadow: 0px 4px 8px 0px rgba(64, 123, 255, 0.34);
   transition: background-color ${({ theme }) => theme.transition.main};
 
@@ -21,13 +22,11 @@ export const ErrorBtn = styled.button`
 `;
 
 export const ErrorImg = styled.img`
-  font-size: 16px;
   width: 280px;
   height: 280px;
   margin: auto;
 
   @media screen and (min-width: ${({ theme }) => theme.breakpoint.tablet}) {
-    font-size: 18px;
     width: 600px;
     height: 500px;
   }
