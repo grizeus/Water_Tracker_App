9855dc2f0597ea2ba7af85bc1b2efb3c6d65d78e
LazyStudent23 <wizard.alex2016@gmail.com>
2025-02-10 22:28:29 +0200

    Update dateUtils.js

--- src/helpers/utils/dateUtils.js
+++ src/helpers/utils/dateUtils.js
@@ -1,18 +1,9 @@
 import { addHours, format, parseISO } from 'date-fns';
-
-export function formatDate(dateString, formatString = 'yyyy-MM-dd') {
-  if (!dateString) return 'Invalid date';
+export function formatDate(dateString, formatString = 'yyyy-MM-dd ') {
   const date = new Date(dateString);
   return format(date, formatString);
 }
-
 export function formatCustomTime(dateString, formatString = 'h:mm a') {
-  if (!dateString) return 'Invalid time'; // Предотвращаем ошибки
-  try {
-    const date = addHours(parseISO(dateString), 0);
-    return format(date, formatString);
-  } catch (error) {
-    console.log('Invalid dateString:', dateString);
-    return 'Invalid time';
-  }
+  const date = addHours(parseISO(dateString), 0);
+  return format(date, formatString);
 }
