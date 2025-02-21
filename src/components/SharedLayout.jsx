56ccf8824ae2cbc7e15aca1feaa2f54e23dc63d9
grizeus <zaytsewww@gmail.com>
2025-02-21 12:12:38 +0200

    delete Wrapper component

--- src/components/SharedLayout.jsx
+++ src/components/SharedLayout.jsx
@@ -3,15 +3,13 @@ import { Suspense } from 'react';
 import { Outlet, useLocation } from 'react-router-dom';
 import { ToastContainer, Zoom } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import { Wrapper } from './Wrapper/Wrapper.jsx';
 
 const SharedLayout = () => {
   const location = useLocation();
   const hideHeaderOnPages = ['/404']; // Сторінки, на яких не відображається Header
 
   return (
-    <Wrapper>
-      {/* <Header /> */}
+    <>
       {!hideHeaderOnPages.includes(location.pathname) && <Header />}
       <main>
         <Suspense fallback={<Loader />}>
@@ -24,7 +22,7 @@ const SharedLayout = () => {
         draggable={false}
         transition={Zoom}
       />
-    </Wrapper>
+    </>
   );
 };
 
