d31ce17905cc6adabe68a1cdd72ce4ac8b7ed6f6
grizeus <zaytsewww@gmail.com>
2025-02-21 21:25:41 +0200

    update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export const content = ["./src/**/*.{js,jsx,ts,tsx}"];
 export const theme = {
-  extend: {},
+  extend: {
+    screens: {
+      sm: "320px",
+      md: "768px",
+      xl: "1440px",
+      "3xl": "2560px",
+    },
+    colors: {
+      "charcoal": "#2f2f2f",
+      "royal": "#407bff",
+      "solitude": "#ecf2ff",
+    },
+  },
 };
 export const plugins = [];
