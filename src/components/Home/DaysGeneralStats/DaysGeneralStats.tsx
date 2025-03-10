a5412bb454621a9c4f686e142549ced1a510cba9
grizeus <32486511+grizeus@users.noreply.github.com>
2025-03-10 14:41:00 +0200

    Fix DaysGeneralStats (#40)
    * fix: improve modal positioning, handle day parsing, add types

---
+++ src/components/Home/DaysGeneralStats/DaysGeneralStats.tsx
@@ -0,0 +1,103 @@
+import { useRef, useEffect } from "react";
+import { formatDate } from "../../../helpers/utils/dateUtils";
+
+interface DayStats {
+  date: string;
+  drinkCount: number;
+  dailyGoal: number | string;
+  waterVolumePercentage: number | string;
+}
+
+interface Position {
+  top: number;
+  left: number;
+  width: number;
+}
+
+const DaysGeneralStats = ({
+  stats,
+  position,
+  isVisible,
+}: {
+  stats: DayStats;
+  position: Position;
+  isVisible: boolean;
+}) => {
+  const { date, drinkCount, dailyGoal, waterVolumePercentage } = stats;
+  const modalRef = useRef<HTMLDivElement>(null);
+  const dayString = date.split("-").pop();
+  if (dayString === undefined) {
+    return;
+  }
+  const day = parseInt(dayString);
+  if (Number.isNaN(day)) {
+    return;
+  }
+
+  useEffect(() => {
+    const modal = modalRef.current;
+    if (!modal) {
+      return;
+    }
+
+    const { top, left, width } = position;
+    const modalWidth = modal.offsetWidth;
+    const modalHeight = modal.offsetHeight;
+    const btnHeight = 58;
+
+    const screenW = window.innerWidth;
+    const isTablet = screenW > 767 && screenW <= 1439;
+    const isDesktop = screenW > 1439;
+
+    modal.style.top = `${top - (modalHeight + btnHeight)}px`;
+
+    if (isTablet) {
+      const shouldAlignLeft = [
+        ...Array.from({ length: 4 }, (_, i) => i + 1),
+        ...Array.from({ length: 4 }, (_, i) => i + 11),
+        ...Array.from({ length: 4 }, (_, i) => i + 21),
+        31,
+      ].includes(day);
+
+      modal.style.left = shouldAlignLeft
+        ? `${left + width / 2}px`
+        : `${left - (modalWidth - width / 2)}px`;
+    }
+    if (isDesktop) {
+      modal.style.left = `${left + width / 2}px`;
+    }
+  }, [position, day]);
+
+  const formattedDate = formatDate(date, "d, MMMM");
+  return (
+    <div
+      className={`${!isVisible ? "hidden" : null} absolute -left-2 w-[280px] rounded-[10px] bg-white px-4 py-6 shadow-[0_4px_4px_0_rgba(64,123,255,0.3)] md:w-[292px]`}
+      ref={modalRef}>
+      <ul className="flex flex-col gap-4">
+        <li className="flex items-center gap-1.5 text-base leading-5">
+          <p className="text-royal">{formattedDate}</p>
+        </li>
+        <li className="flex items-center gap-1.5 text-base leading-5">
+          Daily norma:
+          <span className="text-lg font-medium leading-6 text-royal">
+            {dailyGoal !== 0 ? dailyGoal : "2.0 L"}
+          </span>
+        </li>
+        <li className="flex items-center gap-1.5 text-base leading-5">
+          Fulfillment of the daily norm:
+          <span className="text-lg font-medium leading-6 text-royal">
+            {waterVolumePercentage}
+          </span>
+        </li>
+        <li className="flex items-center gap-1.5 text-base leading-5">
+          How many servings of water:
+          <span className="text-lg font-medium leading-6 text-royal">
+            {drinkCount}
+          </span>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default DaysGeneralStats;
